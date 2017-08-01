de5d1638e8d0079499ac616d56e5dfca1c08fb8c
Keelin Becker-Wheeler <keelinwheeler@gmail.com>
2017-08-01 19:56:57 -0400

    Added card placeholder for drop zones

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -1,30 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
     public Transform homeParent = null;
+    public Transform placeHolderParent = null;
+    public GameObject placeHolder = null;
+
     public enum Type { DEFAULT };
     public Type type = Type.DEFAULT;
 
 	public void OnBeginDrag(PointerEventData eventData) {
 		// Debug.Log ("OnBeginDrag");
+
         homeParent = this.transform.parent;
+        placeHolderParent = homeParent;
+
+        placeHolder = new GameObject();
+        placeHolder.transform.SetParent(placeHolderParent);
+        LayoutElement le = placeHolder.AddComponent<LayoutElement>();
+        le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+        le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+        le.flexibleWidth = 0;
+        le.flexibleHeight = 0;
+
+        placeHolder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+
         this.transform.SetParent(this.transform.parent.parent);
+
         GetComponent<CanvasGroup>().blocksRaycasts = false;
 	}
 
 	public void OnDrag(PointerEventData eventData) {
 		// Debug.Log ("OnDrag");
+
         this.transform.position = eventData.position;
+
+        if (placeHolder.transform.parent != placeHolderParent)
+            placeHolder.transform.SetParent(placeHolderParent);
+
+        int newSiblingIndex = placeHolderParent.childCount;
+        for (int i = 0; i < placeHolderParent.childCount; i++) {
+            if (this.transform.position.x < placeHolderParent.GetChild(i).position.x) {
+                newSiblingIndex = i;
+
+                if (placeHolder.transform.GetSiblingIndex() < newSiblingIndex)
+                    i--;
+
+                break;
+            }
+        }
+        placeHolder.transform.SetSiblingIndex(newSiblingIndex);
 	}
 
 	public void OnEndDrag(PointerEventData eventData) {
 		// Debug.Log ("OnEndDrag");
+
         this.transform.SetParent(homeParent);
+        this.transform.SetSiblingIndex(placeHolder.transform.GetSiblingIndex());
         GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        Destroy(placeHolder);
 	}
 
 	// Use this for initialization
--- Assets/DropZone.cs
+++ Assets/DropZone.cs
@@ -9,12 +9,24 @@ public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
 
     public void OnPointerEnter(PointerEventData eventData) {
         // Debug.Log ("OnPointerEnter to " + gameObject.name);
+
+        if (eventData.pointerDrag == null) return;
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null) {
+            /* Only accept draggables of filtered type but let DEFAULT drop zones accept all */
+            if (d.type == this.type || this.type == Draggable.Type.DEFAULT)
+                d.placeHolderParent = this.transform;
+        }
     }
 
     public void OnDrop(PointerEventData eventData) {
         // Debug.Log (eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+        if (eventData.pointerDrag == null) return;
+
         Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
-        if (d != null) {
+        if (d != null && d.placeHolderParent == this.transform) {
             /* Only accept draggables of filtered type but let DEFAULT drop zones accept all */
             if (d.type == this.type || this.type == Draggable.Type.DEFAULT)
                 d.homeParent = this.transform;
@@ -23,5 +35,14 @@ public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
 
     public void OnPointerExit(PointerEventData eventData) {
         // Debug.Log ("OnPointerExit to " + gameObject.name);
+
+        if (eventData.pointerDrag == null) return;
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null) {
+            /* Only accept draggables of filtered type but let DEFAULT drop zones accept all */
+            if (d.type == this.type || this.type == Draggable.Type.DEFAULT)
+                d.placeHolderParent = d.homeParent;
+        }
     }
 }
